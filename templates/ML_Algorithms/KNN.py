a13611de32d9e73d6427fdb9b7e3fefbd2b38bcf
Syd <sydneychan2003@gmail.com>
2023-12-07 20:04:12 -0500

    phase 3

---
+++ templates/ML_Algorithms/KNN.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import LabelEncoder
+#KNN to classify missing race values
+#SKlearn KNN 
+def sklearn_KNN(X_train,X_test,Y_train,k):
+    model = KNeighborsClassifier(n_neighbors=k)
+    model.fit(X_train,Y_train)
+    predictions = model.predict(X_test)
+    return predictions
+
+#Read data
+data = pd.read_csv('cleaneddata.csv')
+k = 5
+def knn(data):
+    #Grouping rows with unknown races 
+    unknownRace = data.loc[data['PERP_RACE'] == 'UNKNOWN'].dropna()
+    knownRace = data.loc[data['PERP_RACE'] != 'UNKNOWN'].dropna()
+    X_test = unknownRace[['AGE_GROUP','OFNS_DESC']].to_numpy()
+    X_train = knownRace[['AGE_GROUP','OFNS_DESC']].to_numpy()
+    Y_train = knownRace['PERP_RACE'].to_numpy()
+
+    
+    # Label encoding for Age Group and Crime Description
+    age_group_encoder = LabelEncoder()
+    ofns_desc_encoder = LabelEncoder()
+    #Encodes labels of column 0 and 1 (age group and crime description)
+    X_train[:, 0] = age_group_encoder.fit_transform(X_train[:, 0])
+    X_test[:, 0] = age_group_encoder.transform(X_test[:, 0])
+    X_train[:, 1] = ofns_desc_encoder.fit_transform(X_train[:, 1])
+    X_test[:, 1] = ofns_desc_encoder.transform(X_test[:, 1])
+
+    # Predict criminal race using KNN
+    predictions = sklearn_KNN(X_train, X_test, Y_train, 5)
+
+    #Declare plot size and dictionary for x and y axis
+    plt.figure(figsize=(11, 12))
+    crime_desc_labels = {}
+    for desc in enumerate(knownRace['OFNS_DESC'].unique()):
+        crime_desc_labels[desc[0]] = desc[1]
+    age_group_labels = {
+        0: '<18',
+        1: '18-24',
+        2: '25-44',
+        3: '45-64',
+        4: '65+' 
+    }
+    #Create scatter plot with different colors for each race
+    for label in set(predictions):
+        mask = predictions == label
+        plt.scatter(X_test[mask, 0], X_test[mask, 1], label=label, alpha=0.8)
+
+    # Get labels for x and y
+    plt.xticks(range(5), [age_group_labels[i] for i in range(5)])
+    plt.yticks(range(len(crime_desc_labels)), [crime_desc_labels[i] for i in range(len(crime_desc_labels))])
+    # Add labels and legend
+    plt.xlabel('Age Group')
+    plt.ylabel('Crime Description')
+    plt.title('Predicted Criminal Race Based on Age Group and Crime Description')
+    plt.legend(loc='best')
+
+    # Show the plot
+    plt.show()
---
+++ templates/ML_Algorithms/Log_Reg.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from datetime import datetime
+#Logistic Regression
+data = pd.read_csv('NYPD_Arrest_Data__Year_to_Date_.csv')
+def log_reg(data): 
+    data['ARREST_DATE'] = pd.to_datetime(data['ARREST_DATE']).dt.date
+    daily_crime_counts = data.groupby('ARREST_DATE').size().reset_index(name='NUM_OF_CRIMES')
+
+    x = pd.to_datetime(daily_crime_counts['ARREST_DATE']).map(lambda x: x.toordinal()).values.reshape(-1,1)
+    y = daily_crime_counts['NUM_OF_CRIMES']
+
+    X_train, X_test, y_train, y_test = train_test_split(x, y)
+
+    linreg = LinearRegression()
+    linreg.fit(X_train, y_train)
+    predictions = linreg.predict(X_test)
+
+    X_test_dates = [datetime.fromordinal(int(date)) for date in X_test.flatten()]
+    plt.scatter(X_test_dates, y_test, color='black', label='Values')
+    plt.plot(X_test_dates, predictions, color='red', linewidth=3, label='Line of Best Fit')
+    plt.xlabel('Dates')
+    plt.ylabel('Number of Crimes')
+    plt.legend()
+    plt.title('Linear Regression: Number of Crimes Based on Date')
+    plt.show()
